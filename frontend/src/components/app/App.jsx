5ab1d8c6a90e77ee6bd1ee98e0a29756e5a6eb8f
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-05-29 15:48:38 -0300

    feat: add dashboard page as index path

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyle } from "../../assets/styles/globalStyle";
 import { QueryClientContextProvider } from "../../contexts";
@@ -6,6 +7,13 @@ import { Dashboard } from "../../pages";
 import { Header } from "../header/Header";
 
 function App() {
+	useEffect(() => {
+		const baseUrl = "http://localhost:5173/";
+		if (window.location.href === baseUrl) {
+			window.location.href = `${baseUrl}dashboard`;
+		}
+	}, []);
+
 	return (
 		<QueryClientContextProvider>
 			<ToastContainer />
@@ -15,7 +23,7 @@ function App() {
 				<Header />
 
 				<Routes>
-					<Route path="/dashboard" element={<Dashboard />} />
+					<Route index path="/dashboard" element={<Dashboard />} />
 				</Routes>
 			</BrowserRouter>
 		</QueryClientContextProvider>
