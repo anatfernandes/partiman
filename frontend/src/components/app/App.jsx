2237c31f781514c407c573326b917ad7a574a3d0
Ana Luiza Fernandes <analuizateixeirafernandes184@gmail.com>
2023-05-29 15:19:13 -0300

    feat: add dashboard page

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyle } from "../../assets/styles/globalStyle";
 import { QueryClientContextProvider } from "../../contexts";
 import { ToastContainer } from "../../hooks";
+import { Dashboard } from "../../pages";
 import { Header } from "../header/Header";
 
 function App() {
@@ -12,8 +13,10 @@ function App() {
 
 			<BrowserRouter>
 				<Header />
-<main>aaaa</main>
-				<Routes></Routes>
+
+				<Routes>
+					<Route path="/dashboard" element={<Dashboard />} />
+				</Routes>
 			</BrowserRouter>
 		</QueryClientContextProvider>
 	);
